b3498e711b3209254c50d84b85afd041ad5a6f78
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-21 22:46:45 -0600

    avance

--- src/graphics/Rectangulo.java
+++ src/graphics/Rectangulo.java
@@ -1,5 +1,8 @@
 package graphics;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  * @author Héctor Quej Cosgaya
  * @author Jose Aguilar Canepa
@@ -7,5 +10,30 @@ package graphics;
  * Esta clase necesita de tu ayuda!
  */
 public class Rectangulo extends FiguraGrafica {
+
+    @Override
+    public void dibujar(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void encojer(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void agrandar(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void cambiarColor(Graphics g, Color c) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mover(Graphics g, String direccion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
